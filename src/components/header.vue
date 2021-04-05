891c0fb480d250ba709b759bea8fe8f744fd787e
chenshujing <812023308@qq.com>
2021-04-05 17:32:48 +0800

    a

---
+++ src/components/header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container_header">
+        <div class="box_wrapper">
+            <div class="header_item">
+                <img src="" alt="">
+            </div>
+            <div class="header_item nav_bar">
+                <div>首页</div>
+                <div>订购活动</div>
+                <div>机构数据</div>
+            </div>
+            <div class="header_item">
+                <div class="login">登录</div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lange="scss" scoped>
+.container_header{
+    
+}
+</style>
